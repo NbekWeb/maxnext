ea19f29c7eea44781d323640f65626716fb63be8
NbekWeb <nbekdev@gmail.com>
2025-01-12 22:59:13 +0500

    fix

--- app/page.jsx
+++ app/page.jsx
@@ -4,27 +4,33 @@ import { useEffect } from "react";
 import "./styles/index.scss";
 import "swiper/css";
 import "swiper/css/navigation";
-import Home from "./components/home";
+
+import dynamic from "next/dynamic";
+
+const Home = dynamic(() => import("./components/home"), { ssr: false });
 
 export default function Page() {
   useEffect(() => {
-    function setViewportWidth() {
-      document.documentElement.style.setProperty(
-        "--viewport-width",
-        window.innerWidth
-      );
-      document.documentElement.style.setProperty(
-        "--scrollbar-width",
-        window.innerWidth - document.documentElement.clientWidth
-      );
+    if (typeof window !== "undefined") {
+      // This ensures the code is only executed on the client-side
+      function setViewportWidth() {
+        document.documentElement.style.setProperty(
+          "--viewport-width",
+          window.innerWidth
+        );
+        document.documentElement.style.setProperty(
+          "--scrollbar-width",
+          window.innerWidth - document.documentElement.clientWidth
+        );
+      }
+
+      setViewportWidth();
+      window.addEventListener("resize", setViewportWidth);
+
+      return () => {
+        window.removeEventListener("resize", setViewportWidth);
+      };
     }
-
-    setViewportWidth();
-    window.addEventListener("resize", setViewportWidth);
-
-    return () => {
-      window.removeEventListener("resize", setViewportWidth);
-    };
   }, []);
 
   return <Home />;
